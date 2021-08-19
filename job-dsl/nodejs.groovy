a01da5e4ef2dc50ac3ae508605878be66042b486
Carlos Yucra <48326936+bcarlog@users.noreply.github.com>
2021-08-18 21:08:38 -0500

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example2') {
+job('NodeJS example') {
     scm {
         git('git://github.com/bcarlog/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
